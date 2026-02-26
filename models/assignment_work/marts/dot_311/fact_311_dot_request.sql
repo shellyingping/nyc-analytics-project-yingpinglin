fedd79f3e33fb0b4e31d7e28cd6468c5ff8fd3a3
Yingping Lin <shellyingping@gmail.com>
2026-02-26 04:30:08 +0000

    added fact 311_doct_request

--- models/assignment_work/marts/dot_311/fact_311_dot_request.sql
+++ models/assignment_work/marts/dot_311/fact_311_dot_request.sql
@@ -1,30 +1,20 @@
--- Fact table: 311 DOT Requests
--- Grain: one row per request_id
--- This model references dimension tables so the DAG shows: staging -> dims -> fact
+{{ config(schema='nyc_transit_restaurants_marts') }}
 
 WITH base AS (
-  SELECT *
-  FROM {{ ref('stg_nyc_311_dot') }}
+  SELECT * FROM {{ ref('stg_nyc_311_dot') }}
 ),
-
 date_dim AS (
-  SELECT *
-  FROM {{ ref('dim_date') }}
+  SELECT * FROM {{ ref('dim_date') }}
 ),
-
 location_dim AS (
-  SELECT *
-  FROM {{ ref('dim_location') }}
+  SELECT * FROM {{ ref('dim_location') }}
 )
 
 SELECT
   b.request_id,
-
-  -- Join keys from dimensions
   d.date_key,
   l.location_key,
 
-  -- Core attributes
   b.created_date,
   b.closed_date,
   b.status,
@@ -34,10 +24,9 @@ SELECT
   b.descriptor,
   b.method_of_submission,
   b._stg_loaded_at
-
 FROM base b
 LEFT JOIN date_dim d
-  ON DATE(b.created_date) = d.full_date
+  ON FORMAT_DATE('%Y%m%d', DATE(b.created_date)) = d.date_key
 LEFT JOIN location_dim l
   ON b.borough = l.borough
  AND ( (b.incident_zip IS NULL AND l.incident_zip IS NULL) OR b.incident_zip = l.incident_zip )
