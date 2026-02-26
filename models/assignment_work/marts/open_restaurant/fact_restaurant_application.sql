885ae51f124205dee04a539b956c84144d1f1adb
Yingping Lin <shellyingping@gmail.com>
2026-02-26 04:30:41 +0000

    added fact_restaurant application

--- models/assignment_work/marts/open_restaurant/fact_restaurant_application.sql
+++ models/assignment_work/marts/open_restaurant/fact_restaurant_application.sql
@@ -1,5 +1,4 @@
--- Fact table: Open Restaurant Applications
--- Grain: one row per record from stg_nyc_open_restaurant_apps
+{{ config(schema='nyc_transit_restaurants_marts') }}
 
 WITH apps AS (
   SELECT *
@@ -7,13 +6,8 @@ WITH apps AS (
 )
 
 SELECT
-  -- Deterministic surrogate key from the whole row (BigQuery-safe)
   CAST(FARM_FINGERPRINT(TO_JSON_STRING(apps)) AS STRING) AS restaurant_application_sk,
-
-
-
   CAST(NULL AS STRING) AS date_key,
   CAST(NULL AS STRING) AS location_key,
-
   apps.*
 FROM apps
